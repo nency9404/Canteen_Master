358e49bee029ef15547308309fcd5ae1ee1d658d
ShahHetviIT <hetvi4551@gmail.com>
2024-02-29 18:13:21 +0530

    spring-boot setup commit

---
+++ server/src/main/java/com/canteenMaster/model/User.java
@@ -0,0 +1,24 @@
+package com.canteenMaster.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String fullName;
+    private String email;
+    private String password;
+    private USER_ROLE role;
+}
