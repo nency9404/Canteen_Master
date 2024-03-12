e6d3c8f659c3e6364b4e92f0a6d70407091e39b8
ShahHetviIT <hetvi4551@gmail.com>
2024-03-12 10:53:45 +0530

    commit

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -8,7 +8,7 @@ export default function Cart() {
   const cart = store.getState().cart;
 
   console.log(cart);
- 
+
   return (
     <Fragment>
       <main className="lg:flex justify-around">
@@ -24,30 +24,30 @@ export default function Cart() {
           <div className="billDetails px-5 text-sm">
             <p className="font-extralight py-5">Bill Details</p>
 
-            {cart.cart.total?
+            {cart.cart.total ? (
               <div className="space-y-3">
-              <div className="flex justify-between text-gray-400">
-                <p>Item Total</p>
-                <p>₹{cart.cart.total}</p>
-              </div>
-              <div className="flex justify-between text-gray-400">
-                <p>Platform Fee</p>
-                <p>₹21</p>
-              </div>
-              <div className="flex justify-between text-gray-400">
-                <p>GST and Restaurant Charges</p>
-                <p>₹33</p>
-              </div>
-              <Divider/>
+                <div className="flex justify-between text-gray-400">
+                  <p>Item Total</p>
+                  <p>₹{cart.cart.total}</p>
+                </div>
+                <div className="flex justify-between text-gray-400">
+                  <p>Platform Fee</p>
+                  <p>₹21</p>
+                </div>
+                <div className="flex justify-between text-gray-400">
+                  <p>GST and Restaurant Charges</p>
+                  <p>₹33</p>
+                </div>
+                <Divider />
 
-              <div className="flex justify-between text-gray-400">
-
-                <p>Total Pay</p>
-                <p>₹{cart.cart.total+21+33}</p>
+                <div className="flex justify-between text-gray-400">
+                  <p>Total Pay</p>
+                  <p>₹{cart.cart.total + 21 + 33}</p>
+                </div>
               </div>
-            </div>:
-            <div>No Items</div>
-            }
+            ) : (
+              <div>No Items</div>
+            )}
           </div>
         </section>
       </main>
